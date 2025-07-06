d1f859ae54f6ff64aef3261c9d942bfe0e38659f
jichavezv <chavezvelajuanignacio@gmail.com>
2025-07-06 00:38:02 -0600

    [FIX] Update imports on Task entity.

--- src/main/java/com/task/tasks/dto/Task.java
+++ src/main/java/com/task/tasks/dto/Task.java
@@ -1,9 +1,9 @@
 package com.task.tasks.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +14,7 @@ import lombok.AllArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Task{
-    @jakarta.persistence.Id
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
